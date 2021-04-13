fd9be80d1bd463f7d066876e4886d0aec561adf7
HomayounFakhar <homayounfakhar@yahoo.com>
2021-04-13 01:02:29 -0400

    HomePage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Typography } from 'antd';
+import { Typography ,Button} from 'antd';
+import { Image } from 'antd';
+import Online from './../img/Online.png'
+
 
 
 const { Text ,Title}  = Typography;
 
+
+
 function Page404() {
+
 
   return (
     <div>
+         <Image
+      width={200}
+      src={Online}
+    />
     <Title level={2}>Welcome To Homayoun Store</Title>
+    <Button type="primary" >Start shopping</Button>   
+    <p></p> 
     </div>
   )};
 
