44af750f7510d7c572aa42df1091e86f9846919e
HomayounFakhar <homayounfakhar@yahoo.com>
2021-04-20 03:15:24 -0400

    Add - Delete Firebase

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -7,17 +7,49 @@ import { Image } from 'antd';
 
   
 const { Text } = Typography;
-
 const columns = [
   {
-    title: 'ProductName',
-    dataIndex: 'ProductName',
+   
+    title: 'Product Code',
+    dataIndex: 'ProductCode',
     sorter: (a, b) => a.productcode - b.productcode,
     sortDirections: ['descend', 'ascend'],
     render: text => <Text type="warning">{text}</Text>,
     
   },
+  {
+    title: 'Product Name',
+    dataIndex: 'ProductName',
+    sorter: (a, b) => a.productName.length - b.productName.length,
+    sortDirections: ['descend', 'ascend'],
+  },
+  {
+    title: 'Old Price',
+    dataIndex: 'OldPrice',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.Price - b.Price,
+    render: text => <Text delete>{text}</Text>,      
+  },        
+  {
+    title: 'Price',
+    dataIndex: 'Price',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.Price - b.Price,
+  },
+  {
+    title: 'Description',
+    dataIndex: 'Description',
+    sorter: (a, b) => a.description.length - b.description.length,
+    sortDirections: ['descend', 'ascend'],  
+  },
+  {
+    title: 'id',
+    dataIndex: 'id',
+    sorter: (a, b) => a.description.length - b.description.length,
+    sortDirections: ['descend', 'ascend'],  
+  },
 ];
+
 
 // Get Data From Firebase 
 function Product() {
@@ -32,11 +64,41 @@ function Product() {
     fetchData()
   } , [] )
 
+  const OnAdd = () => 
+  {
+  const db = firebase.firestore()
+  db.collection("Products").doc("4").add ({
+    ProductCode : 1 ,
+    ProductName : "Add on Function",
+    
+  })
+  }
 
 return(
   
   <> 
-  <Table columns={columns} dataSource={spells}/>
+  <button onClick={OnAdd}>OnAdd</button>
+  <Table columns={columns} dataSource={spells}
+   onRow={(record, rowIndex) => {
+    return {
+      onClick: event => {
+        console.log(record.ProductCode)
+        const db = firebase.firestore()
+        db.collection("Products").doc(record.ProductCode).delete()
+        console.log(" spells.ProductCode  = "+record.ProductCode)        
+        console.log("Delete")           
+      }, // click row
+
+      onDoubleClick: event => {
+     
+      }, // double click row
+      onContextMenu: event => {}, // right button click row
+      onMouseEnter: event => {}, // mouse enter row
+      onMouseLeave: event => {}, // mouse leave row
+    };
+  }}
+
+  />
   </>
     
   );
