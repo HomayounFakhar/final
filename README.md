6346573b1fa7c08bf95d442b116b860f3305c1e1
HomayounFakhar <homayounfakhar@yahoo.com>
2021-04-19 21:50:37 -0400

    firebase

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ Web store web store
 
 How to install ant.design
 https://ant.design/docs/react/use-with-create-react-app
+
+
+API
+yarn add axios
+---
+
+
+npm install react-axios
+
+npm install axios
+npm install react
+
+Optional
+npm install prop-types
+
+
+npm install firestore-export-import
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import axios from 'axios'
+import firebase from './firebase'
 
+import { Table ,Typography} from 'antd';
+import { Image } from 'antd';
+import Online from './../img/Online.png'
+  
+const { Text } = Typography;
 
+  // Test Firebase 
 function Product() {
+  const [spells, setSpells] = React.useState([])
+  
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore()
+      const data = await db.collection("Products").get()
+      setSpells(data.docs.map(doc => doc.data()))
+    }
+    fetchData()
+  } , [] )
 
+return(
+  <ul>
+  {spells.map(spell => (
+  <li>{spell.ProductName}</li>
+))}
+</ul>
+    );
 
-  return (
-    <div>
-         <p>Test Product</p>
-    </div>
-  )};
-
+  }
+  
   export default Product;
 
 
